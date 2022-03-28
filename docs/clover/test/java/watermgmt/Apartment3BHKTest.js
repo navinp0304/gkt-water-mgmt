63c9b2d69e318d07f63979a2176f701c48de6979
Navin P <navinp0304@gmail.com>
2022-03-28 16:00:56 +0530

    copying the reports into main/docs for site serving.

---
+++ docs/clover/test/java/watermgmt/Apartment3BHKTest.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":92,"id":904,"methods":[{"el":17,"sc":2,"sl":14},{"el":43,"sc":2,"sl":19},{"el":49,"sc":2,"sl":46},{"el":54,"sc":2,"sl":51},{"el":62,"sc":2,"sl":56},{"el":84,"sc":2,"sl":64},{"el":91,"sc":2,"sl":86}],"name":"Apartment3BHKTest","sl":11}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {"test_12":{"methods":[{"sl":86}],"name":"testApartment3BHKIntegerString","pass":true,"statements":[{"sl":88},{"sl":90}]},"test_23":{"methods":[{"sl":56}],"name":"testCloneIntegerDouble","pass":true,"statements":[{"sl":58},{"sl":59},{"sl":60}]},"test_42":{"methods":[{"sl":51}],"name":"testApartment3BHKDefaultCost","pass":true,"statements":[{"sl":53}]},"test_48":{"methods":[{"sl":46}],"name":"testGetDefaultLitres","pass":true,"statements":[{"sl":48}]},"test_6":{"methods":[{"sl":64}],"name":"testApartment3BHKIntegerDouble","pass":true,"statements":[{"sl":66},{"sl":68},{"sl":73},{"sl":74},{"sl":76},{"sl":79},{"sl":80},{"sl":82}]},"test_9":{"methods":[{"sl":19}],"name":"testAllCommands","pass":true,"statements":[{"sl":21},{"sl":22},{"sl":23},{"sl":24},{"sl":25},{"sl":26},{"sl":27},{"sl":28},{"sl":29},{"sl":31},{"sl":32},{"sl":33},{"sl":35},{"sl":38},{"sl":39},{"sl":41}]}}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [9], [], [9], [9], [9], [9], [9], [9], [9], [9], [9], [], [9], [9], [9], [], [9], [], [], [9], [9], [], [9], [], [], [], [], [48], [], [48], [], [], [42], [], [42], [], [], [23], [], [23], [23], [23], [], [], [], [6], [], [6], [], [6], [], [], [], [], [6], [6], [], [6], [], [], [6], [6], [], [6], [], [], [], [12], [], [12], [], [12], [], []]
---
+++ docs/clover/treemap-json.js
@@ -0,0 +1,63 @@
+var treeMapJson = {"id":"Clover database Mon Mar 28 2022 15:03:07 IST0","name":"","data":{
+    "$area":373.0,"$color":100.0,"title":" 373 Elements, 100% Coverage"},
+  "children":[{"id":"main.java.watermgmt0","name":"main.java.watermgmt",
+      "data":{"$area":373.0,"$color":100.0,"title":
+        "main.java.watermgmt 373 Elements, 100% Coverage"},"children":[{"id":
+          "ApartmentBase0","name":"ApartmentBase","data":{"$area":84.0,
+            "$color":100.0,"path":
+            "main/java/watermgmt/ApartmentBase.html#ApartmentBase","title":
+            "ApartmentBase 84 Elements, 100% Coverage"},"children":[]},{"id":
+          "SlabInterval84","name":"SlabInterval","data":{"$area":43.0,
+            "$color":100.0,"path":
+            "main/java/watermgmt/SlabInterval.html#SlabInterval","title":
+            "SlabInterval 43 Elements, 100% Coverage"},"children":[]},{"id":
+          "WaterManagement127","name":"WaterManagement","data":{"$area":
+            4.0,"$color":100.0,"path":
+            "main/java/watermgmt/WaterManagement.html#WaterManagement",
+            "title":"WaterManagement 4 Elements, 100% Coverage"},"children":[]},
+        {"id":"AddGuestsCommand131","name":"AddGuestsCommand","data":{
+            "$area":37.0,"$color":100.0,"path":
+            "main/java/watermgmt/AddGuestsCommand.html#AddGuestsCommand",
+            "title":"AddGuestsCommand 37 Elements, 100% Coverage"},
+          "children":[]},{"id":"CorporationRate168","name":
+          "CorporationRate","data":{"$area":8.0,"$color":100.0,"path":
+            "main/java/watermgmt/CorporationRate.html#CorporationRate",
+            "title":"CorporationRate 8 Elements, 100% Coverage"},"children":[]},
+        {"id":"Apartment2BHK176","name":"Apartment2BHK","data":{"$area":
+            18.0,"$color":100.0,"path":
+            "main/java/watermgmt/Apartment2BHK.html#Apartment2BHK","title":
+            "Apartment2BHK 18 Elements, 100% Coverage"},"children":[]},{"id":
+          "AllotWaterCommand194","name":"AllotWaterCommand","data":{"$area":
+            63.0,"$color":100.0,"path":
+            "main/java/watermgmt/AllotWaterCommand.html#AllotWaterCommand",
+            "title":"AllotWaterCommand 63 Elements, 100% Coverage"},
+          "children":[]},{"id":"ICommand257","name":"ICommand","data":{
+            "$area":0.0,"$color":-100.0,"path":
+            "main/java/watermgmt/ICommand.html#ICommand","title":
+            "ICommand 0 Elements,  -  Coverage"},"children":[]},{"id":
+          "CommandParser257","name":"CommandParser","data":{"$area":48.0,
+            "$color":100.0,"path":
+            "main/java/watermgmt/CommandParser.html#CommandParser","title":
+            "CommandParser 48 Elements, 100% Coverage"},"children":[]},{"id":
+          "Apartment3BHK305","name":"Apartment3BHK","data":{"$area":18.0,
+            "$color":100.0,"path":
+            "main/java/watermgmt/Apartment3BHK.html#Apartment3BHK","title":
+            "Apartment3BHK 18 Elements, 100% Coverage"},"children":[]},{"id":
+          "BorewellRate323","name":"BorewellRate","data":{"$area":8.0,
+            "$color":100.0,"path":
+            "main/java/watermgmt/BorewellRate.html#BorewellRate","title":
+            "BorewellRate 8 Elements, 100% Coverage"},"children":[]},{"id":
+          "BillCommand331","name":"BillCommand","data":{"$area":23.0,
+            "$color":100.0,"path":
+            "main/java/watermgmt/BillCommand.html#BillCommand","title":
+            "BillCommand 23 Elements, 100% Coverage"},"children":[]},{"id":
+          "IRateCalculator354","name":"IRateCalculator","data":{"$area":
+            0.0,"$color":-100.0,"path":
+            "main/java/watermgmt/IRateCalculator.html#IRateCalculator",
+            "title":"IRateCalculator 0 Elements,  -  Coverage"},"children":[]},
+        {"id":"TankerRate354","name":"TankerRate","data":{"$area":19.0,
+            "$color":100.0,"path":
+            "main/java/watermgmt/TankerRate.html#TankerRate","title":
+            "TankerRate 19 Elements, 100% Coverage"},"children":[]}]}]}
+;
+processTreeMapJson (treeMapJson);
