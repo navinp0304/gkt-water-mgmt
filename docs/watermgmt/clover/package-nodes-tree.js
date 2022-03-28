9246442497578e79c5c3d9437a6b324d1bdf6902
Navin P <navinp0304@gmail.com>
2022-03-28 16:35:57 +0530

    copying reports into watermgmt

---
+++ docs/watermgmt/clover/package-nodes-tree.js
@@ -0,0 +1,47 @@
+ 
+
+var Packages = {
+    nodes: [
+                                                                                                                        
+                
+{
+    "id": "main.java.watermgmt",
+    "text": "main.java.watermgmt",
+    "package": "main.java.watermgmt",
+    "url": "main/java/watermgmt/pkg-summary.html",
+            "coverage": "100%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "main/java/watermgmt/pkg-summary.html"},
+    "children": [
+                    ]
+},
+                                            
+                
+{
+    "id": "test.java.watermgmt",
+    "text": "test.java.watermgmt",
+    "package": "test.java.watermgmt",
+    "url": "test/java/watermgmt/testsrc-pkg-summary.html",
+            "coverage": "98.9%",
+        "icon": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed",
+            "li_attr": {"data-is-link": "true"},
+        "a_attr": {"href": "test/java/watermgmt/testsrc-pkg-summary.html"},
+    "children": [
+                    ]
+},
+                        ],
+    settings: {
+        "icons": {
+            "package": {
+                "open": "aui-icon aui-icon-small aui-iconfont-devtools-folder-open",
+                "closed": "aui-icon aui-icon-small aui-iconfont-devtools-folder-closed"
+            },
+            "state": {
+                "collapsed": "aui-icon aui-icon-small aui-iconfont-collapsed",
+                "expanded": "aui-icon aui-icon-small aui-iconfont-expanded",
+                "forRemoval": "hidden aui-iconfont-collapsed aui-iconfont-expanded"
+            }
+        }
+    }
+};
