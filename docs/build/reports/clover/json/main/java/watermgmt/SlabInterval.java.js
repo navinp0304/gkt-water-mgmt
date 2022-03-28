f93fcdb41b3fa2bd2b161b43855bc22090632ac7
Navin P <navinp0304@gmail.com>
2022-03-28 15:32:54 +0530

    adding hardcoded value build

---
+++ docs/build/reports/clover/json/main/java/watermgmt/SlabInterval.java.js
@@ -0,0 +1,19 @@
+processClover (  {
+  "id": "main_java_watermgmt_SlabInterval_java",
+  "lines": [    "",    "",    "",    "",    "",    "",    "",    "511",    "511",    "1",    "510",    "1",    "509",    "1",    "508",    "1",    "",    "507",    "507",    "507",    "",    "",    "182",    "182",    "",    "",    "4",    "4",    "",    "",    "",    "",    "",    "",    "",    "",    "184",    "184",    "1",    "",    "",    "",    "",    "183",    "1",    "",    "182",    "",    "",    "",    "",    "",    "182",    "63",    "",    "",    "",    "",    "",    "182",    "118",    "",    "",    "",    "64",    "",    "",    "",    ""  ],
+  "stats": {
+    "Complexity": 12,
+    "CoveredElements": 43,
+    "ErroneousTests": 0,
+    "FailingTests": 0,
+    "PassingTests": 0,
+    "PcErroneousTests": -1,
+    "PcFailingTests": -1,
+    "PcPassingTests": -1,
+    "TestExecutionTime": 0,
+    "Tests": 0,
+    "TotalPercentageCovered": 100,
+    "UncoveredElements": 0
+  }
+}
+ ); 
---
+++ docs/build/reports/clover/json/main/java/watermgmt/TankerRate.java.js
@@ -0,0 +1,19 @@
+processClover (  {
+  "id": "main_java_watermgmt_TankerRate_java",
+  "lines": [    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "",    "62",    "",    "62",    "1",    "",    "61",    "",    "",    "61",    "178",    "",    "",    "",    "",    "",    "",    "178",    "61",    "",    "",    "",    "",    "61",    "",    "",    "",    "",    "",    "60",    "",    "60",    "1",    "59",    "",    "",    ""  ],
+  "stats": {
+    "Complexity": 5,
+    "CoveredElements": 19,
+    "ErroneousTests": 0,
+    "FailingTests": 0,
+    "PassingTests": 0,
+    "PcErroneousTests": -1,
+    "PcFailingTests": -1,
+    "PcPassingTests": -1,
+    "TestExecutionTime": 0,
+    "Tests": 0,
+    "TotalPercentageCovered": 100,
+    "UncoveredElements": 0
+  }
+}
+ ); 
