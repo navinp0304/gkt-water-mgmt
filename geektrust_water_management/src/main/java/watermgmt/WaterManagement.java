ad6a53c5a2bd2e2c13d0e4973e491e681db50bc6
Navin P <navinp0304@gmail.com>
2022-03-29 09:12:03 +0530

    mvn cleanup and modifications

--- geektrust_water_management/src/main/java/watermgmt/WaterManagement.java
+++ geektrust_water_management/src/main/java/watermgmt/WaterManagement.java
@@ -1,11 +1,25 @@
 package main.java.watermgmt;
 
+import java.io.FileInputStream;
+
 public class WaterManagement {
 
 	public WaterManagement() {
 	}
 
 	public static void main(String[] args) {
+		String filename = args[0];
+		filename = filename.strip();
+		if(filename.length()==0) {
+			return;
+		}
+		FileInputStream instream = null; 
+		try {  
+	          instream = new FileInputStream(filename);  
+	          System.setIn(instream);  
+	      } catch (Exception e) {  
+	          System.err.println("Error Occurred.");  
+	      }  		
 		CommandParser cmdParser = new CommandParser();
 		cmdParser.run();
 
