da7c0aa1912d80d84c0aeae0a74b86d57e425aae
Navin P <navinp0304@gmail.com>
2022-05-01 08:55:06 +0530

    try resource changes

--- gkt-water-mgmt/src/main/java/com/example/geektrust/WaterMgmtLogger.java
+++ gkt-water-mgmt/src/main/java/com/example/geektrust/WaterMgmtLogger.java
@@ -10,44 +10,23 @@ public class WaterMgmtLogger {
 	private static final String fileName = "logFile.txt";
 
 	public String readFirstLine() {
-		BufferedReader brRead = null;
-		String firstLine = null;
-		try {
-			brRead = new BufferedReader(new FileReader(fileName));
-			firstLine = brRead.readLine();
+		String firstLine = "";
 
+		try (BufferedReader brRead = new BufferedReader(new FileReader(fileName))) {
+			firstLine = brRead.readLine();
 		} catch (IOException e1) {
 			e1.printStackTrace();
-		} finally {
-			try {
-				if (brRead != null)
-					brRead.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 		return firstLine;
 
 	}
 
 	public void writeFirstLine(String s) {
-		BufferedWriter brWrite = null;
 
-		try {
-			brWrite = new BufferedWriter(new FileWriter(fileName));
+		try (BufferedWriter brWrite = new BufferedWriter(new FileWriter(fileName))) {
 			brWrite.write(s);
-
 		} catch (IOException e1) {
 			e1.printStackTrace();
-		} finally {
-			try {
-				if (brWrite != null)
-					brWrite.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 	}
 }
